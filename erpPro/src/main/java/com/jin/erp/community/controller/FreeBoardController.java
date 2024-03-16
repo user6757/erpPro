88c0f512683319c0e20ea165747634636d3ff164
dmlwns <dmlwns@192.168.219.106>
2024-03-16 23:13:08 +0900

    나마

--- erpPro/src/main/java/com/jin/erp/community/controller/FreeBoardController.java
+++ erpPro/src/main/java/com/jin/erp/community/controller/FreeBoardController.java
@@ -39,6 +39,7 @@ private static final Logger logger = LoggerFactory.getLogger(HomeController.clas
 	
 	@RequestMapping(value="list", method= {RequestMethod.POST, RequestMethod.GET})
 	public String freelist(Model model, FreeBoardVO freeBoardVO, HttpServletRequest request) {
+		System.out.println("ddsasdsa");
 		System.out.println("ssss");
 		Page paging = new Page();
 		if(request.getParameter("page") != null) {
